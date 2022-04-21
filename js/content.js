8775a7f4d79a4da00d72e841c50451f31236b358
Paul Smith <paulrsmithjnr@gmail.com>
2022-04-21 16:29:35 -0500

    added element to btn div (incomplete)

--- js/content.js
+++ js/content.js
@@ -1,1 +1,26 @@
 console.log("On a Meets Call! - content");
+window.onload = function () {
+    console.log("Fully loaded!");
+    const contentDivClassName = "MCcOAc IqBfM ecJEib EWZcud d8Etdd cjGgHb LcUz9d";
+    const div = document.getElementsByClassName(contentDivClassName);
+    console.log("This is the div:", div[0]);
+
+    const btnsDivClassName = "Tmb7Fd";
+    const btnsDiv = document.getElementsByClassName(btnsDivClassName)[0];
+
+    var pipBtn = document.createElement("div");
+    pipBtn.classList.add("pipBtn");
+
+    // var pipIcon = document.createElement("img");
+
+    // const imgSRC = chrome.runtime.getURL("./img/meets_plus_icon.png");
+    // console.log("imgSRC:", imgSRC);
+
+    // pipIcon.src = imgSRC;
+    // pipIcon.alt = "Pip Icon";
+    // pipBtn.appendChild(pipIcon);
+
+    btnsDiv.insertBefore(pipBtn, btnsDiv.children[0]);
+
+    console.log("btnsDiv:", btnsDiv);
+}
